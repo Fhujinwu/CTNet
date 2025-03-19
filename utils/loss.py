a29b2be9e4d1d880cc76e33015c169b0c6013930
Jinwu Hu <41566334+Fhujinwu@users.noreply.github.com>
2025-03-19 19:25:41 +0800

    update loss.py

---
+++ utils/loss.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/4 15:00
+# @Author  : Jinwu Hu
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+def structure_loss(pred, mask):
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+    pred = torch.sigmoid(pred)
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1 - (inter + 1) / (union - inter + 1)
+
+    return (wbce + wiou).mean()
